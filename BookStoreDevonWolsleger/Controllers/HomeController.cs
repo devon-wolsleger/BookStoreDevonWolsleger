813bd102ec06355988aa3ac868c02fba48145c24
Devon Wolsleger <dwolsleg@byu.edu>
2023-03-01 22:34:29 -0700

    Add project files.

---
+++ BookStoreDevonWolsleger/Controllers/HomeController.cs
@@ -0,0 +1,23 @@
+﻿using BookStoreDevonWolsleger.Models;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BookStoreDevonWolsleger.Controllers
+{
+    public class HomeController : Controller
+    {
+        private BookstoreContext context { get; set; }
+        public HomeController (BookstoreContext temp)
+        {
+            context = temp;
+        }
+        public IActionResult Index()
+        {
+            var blah = context.Books.ToList();
+            return View(blah);
+        }
+    }
+}
