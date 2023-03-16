c6231eae788a1ba27a00a33a9ea82f414389f7fa
Devon Wolsleger <dwolsleg@byu.edu>
2023-03-15 22:30:39 -0600

    Fixed the shopping cart Icon

---
+++ BookStoreDevonWolsleger/Components/CartSummaryViewComponent.cs
@@ -0,0 +1,22 @@
+﻿using BookStoreDevonWolsleger.Models;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BookStoreDevonWolsleger.Components
+{
+    public class CartSummaryViewComponent : ViewComponent
+    {
+        private Basket basket;
+        public CartSummaryViewComponent(Basket b)
+        {
+            basket = b;
+        }
+        public IViewComponentResult Invoke()
+        {
+            return View(basket);
+        }
+    }
+}
